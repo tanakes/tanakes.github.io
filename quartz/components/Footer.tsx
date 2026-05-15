09097b76a9d5f1a5b2a5c6fb46c718257472ec04
liallogic-cyber <liallogic@gmail.com>
2026-05-15 23:53:46 +0500

    update

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,3 +1,16 @@
-const Footer: QuartzComponent = () => {
-  return null
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import style from "./styles/footer.scss"
+import { version } from "../../package.json"
+import { i18n } from "../i18n"
+
+interface Options {
+  links: Record<string, string>
 }
+
+export default ((opts?: Options) => {
+  const Footer: QuartzComponent = () => {
+    return null
+  }
+  Footer.css = style
+  return Footer
+}) satisfies QuartzComponentConstructor
